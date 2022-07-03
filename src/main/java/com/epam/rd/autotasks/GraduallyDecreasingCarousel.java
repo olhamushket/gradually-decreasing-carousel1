5ea9f9ff3d7e36a5802b79aa7309127f6431bb29
Olha Mushket <olhamushket@gmail.com>
2022-07-03 03:56:23 +0300

    Solved

--- src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
@@ -4,4 +4,15 @@ public class GraduallyDecreasingCarousel extends DecrementingCarousel{
     public GraduallyDecreasingCarousel(final int capacity) {
         super(capacity);
     }
+
+    @Override
+    public CarouselRun run(){
+        //throw new UnsupportedOperationException();
+        if (getState() == 1) {
+            return null;
+        }
+        setState((byte) 1);
+        return new CarouselRun(getArr(), getI(), 2);
+    }
 }
+
